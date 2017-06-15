fd8de34ae7405259aced2592cb373a43d63f61f5
Yusuf Saib <yusuf_saib@symantec.com>
2017-06-15 10:22:05 -0700

    setup signal

--- apple/common/Crypto.swift
+++ apple/common/Crypto.swift
@@ -1,12 +1,15 @@
 import Foundation
 import RNCryptor
+import SignalProtocolC
 
 class Crypto {
 
     private let password: String
+    private static var lock = NSRecursiveLock()
 
     init(password: String) {
         self.password = password
+        setupSignal()
     }
 
     func encrypt(data: Data) -> Data {
@@ -19,6 +22,29 @@ class Crypto {
         } catch {
             print(error)
             return nil
+        }
+    }
+
+    func setupSignal() {
+        var globalContext: OpaquePointer?
+        var result: Int32
+
+        result = signal_context_create(&globalContext, &Crypto.lock)
+        checkForError(result: result, name: "signal_context_create")
+
+        var provider = signal_crypto_provider()
+        result = signal_context_set_crypto_provider(globalContext, &provider)
+        checkForError(result: result, name: "signal_context_set_crypto_provider")
+
+        result = signal_context_set_locking_functions(globalContext,
+                                                { _ in Crypto.lock.lock() },
+                                                { _ in Crypto.lock.unlock() })
+        checkForError(result: result, name: "signal_context_set_locking_functions")
+    }
+
+    func checkForError(result: Int32, name: String) {
+        if result != 0 {
+            print("error for \(name): \(result)")
         }
     }
 }
