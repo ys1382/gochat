d00a50a8a00e0e41dff61303748a045c16d0a0ec
sinbadflyce <nqminh.ce@gmail.com>
2017-08-09 14:53:55 +0700

    Rework AV code to make that be mergeable
    - Rollback local server ip

--- apple/common/Network.swift
+++ apple/common/Network.swift
@@ -3,7 +3,7 @@ import Starscream
 
 class Network: WebSocketDelegate {
 
-    static var address = "ws://sinbadflyce.com:8000/ws"
+    static var address = "ws://127.0.0.1:8000/ws"
 
     static let shared = Network()
 
